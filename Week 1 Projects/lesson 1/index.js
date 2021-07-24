d39535857a54cbabea27e30e977d87e2e31f49d9
Jonathan Walsh <87747751+Falidian@users.noreply.github.com>
2021-07-23 23:16:01 -0600

    test

---
+++ Week 1 Projects/lesson 1/index.js
@@ -0,0 +1,11 @@
+var bankAccountBalance = 52.10;
+
+var firstName = "Tom";
+var lastName = 'Sawyer';
+
+var numberOfFriends = 5;
+
+var isHotOutside = false;
+var isHappy = true;
+
+console.log('Hello World!');
